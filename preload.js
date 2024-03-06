218ed688599708b3fec153303c6246add4bb70aa
Shrayus Masanam <45981228+shrayus-masanam@users.noreply.github.com>
2024-03-05 21:09:26 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", () => {
-    var style = document.createElement('style');
-    style.innerHTML = `
-    #titlecard, .app-horizontal-widget-holder, .app-bottom-content-anchor, .app-viewcard-strip, #image-header, .scene-footer-container, #watermark, .id-interactive-hovercard, .hdeJwf {
-      display: none;
-    }
-    `
-    document.body.appendChild(style);
+	var style = document.createElement('style');
+	style.innerHTML = `
+    	#titlecard, .app-horizontal-widget-holder, .app-bottom-content-anchor, .app-viewcard-strip, #image-header, .scene-footer-container, #watermark, .id-interactive-hovercard, .hdeJwf, #omnibox, #omnibox-container {
+    		display: none;
+    	}
+    `;
+	document.body.appendChild(style);
 });
