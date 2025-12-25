7e79c884be64b42ff27ab5aeff052cf4310c286a
Shrayus Masanam <45981228+shrayus-masanam@users.noreply.github.com>
2025-12-25 13:43:57 -0500

    update google maps searchbox selector

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
 	var style = document.createElement('style');
 	style.innerHTML = `
-    	#titlecard, .app-horizontal-widget-holder, .app-bottom-content-anchor, .app-viewcard-strip, #image-header, .scene-footer-container, #watermark, .id-interactive-hovercard, .hdeJwf, #omnibox, #omnibox-container {
+    	#titlecard, .app-horizontal-widget-holder, .app-bottom-content-anchor, .app-viewcard-strip, #image-header, .scene-footer-container, #watermark, .id-interactive-hovercard, .Owrmqf, #omnibox, #omnibox-container {
     		display: none;
     	}
     `;
